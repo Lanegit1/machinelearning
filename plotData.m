afceba29ef4000b61a3a7c5dbb3b6bc8e0f1fd7c
Lanegit1 <78578360+Lanegit1@users.noreply.github.com>
2021-02-05 12:02:23 +0800

    ex2
    吴恩达机器学习第二次练习

--- plotData.m
+++ plotData.m
@@ -1,28 +1,37 @@
-function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
-%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
-%   population and profit.
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
-figure; % open a new figure window
+% Create New Figure
+figure; hold on;
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Plot the training data into a figure using the 
-%               "figure" and "plot" commands. Set the axes labels using
-%               the "xlabel" and "ylabel" commands. Assume the 
-%               population and revenue data have been passed in
-%               as the x and y arguments of this function.
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
 %
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+a=find(y==1);
+b=find(y==0);
+plot(X(a,1),X(a,2),'k+');
+plot(X(b,1),X(b,2),'ko');
+xlabel('Exam 1 score');
+ylabel('Exam 2 score');
+title('Scatter plot of training data');
+set(gca,'xtick',[30:10:100]);
+set(gca,'ytick',[30:10:100]);
 
-plot(x,y,'rx', 'MarkerSize', 10);
-xlabel('population');
-ylabel('revenue');
 
 
-% ============================================================
 
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
 
 end
-#aaa
